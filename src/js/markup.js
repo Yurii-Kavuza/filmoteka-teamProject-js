ccc24d4338942ed07a98e2888913826465425a4f
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2022-08-26 12:26:05 +0300

    add item element

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,51 +1,24 @@
-const headMarkup = item => {
+const headMarkup = item =>
   `<li>
   <div class="slot__thumb">
           <a href="#">
-            <img src="#" alt="#" />
+            <img src="${IMG_URL}${item.poster_path}" alt="${item.title}" />
           </a>
-          <h2>namefilm</h2>
-          <p>ganre</p>
-          <p>2020</p>
+          <h2>${item.title || item.original_title}</h2>
+          <p>${Object.values(item.genre_ids)}</p>
+          <p>${item.release_date.slice(0, 4)}</p>
 		  </div>
         </li>`;
-};
 
-const watchedMarkup = item => {
+const watchedMarkup = item =>
   `<li>
   <div class="slot__thumb">
   <a href="#">
-            <img src="#" alt="#" />
+            <img src="${IMG_URL}${item.poster_path}" alt="${item.title}" />
           </a>
-          <h2>head</h2>
-          <p>genre</p>
-          <p>2020</p>
-          <p>vote</p>
+          <h2>${item.title || item.original_title}</h2>
+          <p>${Object.values(item.genre_ids)}</p>
+          <p>${item.release_date.slice(0, 4)}</p>
+          <p>${item.vote}</p>
 		  </div>
         </li>`;
-};
-
-const modalMarkup = item => {
-  `<img src="#" alt="#" />
-	 <h1>head</h1>
-	 <table>
-        <tr>
-          <td>Vote / Votes</td>
-          <td>7.3 /1260</td>
-        </tr>
-        <tr>
-          <td>Popularity</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Original Title</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Genre</td>
-          <td></td>
-        </tr>
-      </table>
-	 <h2>ABOUT</h2>
-	 <p></p>`;
-};
