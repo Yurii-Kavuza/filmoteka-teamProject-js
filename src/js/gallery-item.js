2817768126eb211aa3a592dd731250d26b232127
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2022-08-26 23:58:21 +0300

    change

--- src/js/gallery-item.js
+++ src/js/gallery-item.js
@@ -29,28 +29,4 @@ const watchedMarkup = item =>
 		  </div>
         </li>`;
 
-const modalMarkup = item =>
-  `<img src="#" alt="#" />
-	 <h1>head</h1>
-	 <table>
-        <tr>
-          <td>Vote / Votes</td>
-          <td>7.3 /1260</td>
-        </tr>
-        <tr>
-          <td>Popularity</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Original Title</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Genre</td>
-          <td></td>
-        </tr>
-      </table>
-	 <h2>ABOUT</h2>
-	 <p></p>`;
-
-export { headMarkup, watchedMarkup, modalMarkup };
+export { headMarkup, watchedMarkup };
