8c0d772d0593193da9bd43f6dc89b73a47b12af3
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2022-08-28 01:31:21 +0300

    add css

--- src/js/gallery-item.js
+++ src/js/gallery-item.js
@@ -1,31 +1,34 @@
 const headMarkup = item =>
   `<li class="gallery__item">
   <div class="slot__thumb">
-            <img src="${item.posterPath}"
+            <img class="img__gallery" src="${item.posterPath}"
              alt="${item.title}"
              data-id="${item.id}"
              data-allGenres="${item.genresAllList}"
              data-overwiew="${item.overview}"
              data-backdrop="${item.backdropPath}"/>
-          <h2>${item.title}</h2>
-          <p>Genre id = ${item.genresShortList}</p>
-          <p>${item.releaseDate}</p>
+          <h2 class="title__gallery">${item.title}</h2>
+          <p class="other__gallery">${item.genresShortList.join(
+            ', '
+          )}<span> | </span>${item.releaseDate}</p>
 		  </div>
         </li>`;
 
 const watchedMarkup = item =>
   `<li class="gallery__item">
   <div class="slot__thumb">
-            <img src="${item.posterPath}"
+            <img class="img__gallery" src="${item.posterPath}"
              alt="${item.title}"
              data-id="${item.id}"
              data-allGenres="${item.genresAllList}"
              data-overwiew="${item.overview}"
              data-backdrop="${item.backdropPath}"/>
-          <h2>${item.title}</h2>
-          <p>Genre id = ${item.genresShortList}</p>
-          <p>${item.releaseDate}</p>
-          <p>${item.vote}</p>
+          <h2 class="title__gallery">${item.title}</h2>
+          <p class="other__gallery">${item.genresShortList.join(
+            ', '
+          )}<span> | </span>${item.releaseDate} <svg id="icon-vote">${
+    item.vote_count
+  }</svg></p>
 		  </div>
         </li>`;
 
