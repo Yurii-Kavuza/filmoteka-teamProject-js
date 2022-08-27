fe6f8e0c8d4aeb6f9164fd448111437bc0e5660f
Yurii-Kavuza <kavuzaiurii@gmail.com>
2022-08-28 00:30:10 +0300

    small update

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const refs = {
 const trendingMovies = new TrendingMovies();
 const startPage = 1;
 trendingMovies.getGenres();
-trendingMovies.getMovies(startPage).then(({ results, total_results, page }) => {
-  const markup = createMarkup(results);
-  refs.listFilm.insertAdjacentHTML('beforeend', markup);
-});
+trendingMovies
+  .getMovies(startPage)
+  .then(({ results, total_results, total_pages, page }) => {
+    const markup = createMarkup(results);
+    refs.listFilm.insertAdjacentHTML('beforeend', markup);
+  });
 
 function createMarkup(cards) {
   return cards.reduce((acc, card) => acc + headMarkup(card), '');
