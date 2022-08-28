2b783532b89e38dd9c57620b3d117925bd70d7cf
Roma Romanenko <romanenko.roma92@gmail.com>
2022-08-28 15:14:39 +0300

    add pagination

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import TrendingMovies from './js/trending-movies';
 import { headMarkup } from './js/markup';
+import { pagination } from './js/pagination';
 import './js/modal-footer';
 const refs = {
   listFilm: document.querySelector('.listFilm'),
@@ -12,9 +13,12 @@ trendingMovies
   .getMovies(startPage)
   .then(({ results, total_results, total_pages, page }) => {
     const markup = createMarkup(results);
+
     refs.listFilm.insertAdjacentHTML('beforeend', markup);
   });
 
 function createMarkup(cards) {
   return cards.reduce((acc, card) => acc + headMarkup(card), '');
 }
+/* примитивный вызов для наглядности рендеринка, позже изменить */
+/* pagination(totalPage, currentPage); */
