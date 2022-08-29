8ab3e3276a997473bb4fac20ce1cc6a10de2b562
Yurii-Kavuza <kavuzaiurii@gmail.com>
2022-08-29 18:54:27 +0300

    pagination is rendering, but does not work

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ trendingMovies
   .getMovies(startPage)
   .then(({ results, total_results, total_pages, page }) => {
     const markup = createMarkup(results);
-
+    pagination(total_pages, page);
     refs.listFilm.insertAdjacentHTML('beforeend', markup);
   });
 
