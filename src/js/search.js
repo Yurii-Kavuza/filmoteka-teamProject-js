5b755743c8d4ebf574ff6d19d8a89d6715282100
Serhii Polishchuk <67332341+psv85@users.noreply.github.com>
2022-08-31 19:02:04 +0300

    trash 2

--- src/js/search.js
+++ src/js/search.js
@@ -1,39 +1,78 @@
 import { Notify } from 'notiflix';
 import API_KEY from './api-key';
-import pagination from './pagination';
+// import pagination from './pagination';
 import { preloaderShow, hidePreloader } from './preloader';
-import headMarkup from './gallery-item';
+// import headMarkup from './gallery-item';
 import searchMovies from './trending-movies';
+import TrendingMovies from './trending-movies';
 // import axios from 'axios';
 // const axios = require('axios');
 
 const form = document.querySelector('.search-form');
 const input = document.querySelector('.search-form__input');
-const gallery = document.querySelector('.container');
+// const gallery = document.querySelector('.container');
+// const trendingMovies = new TrendingMovies();
+// const searchMovies = new searchMovies();
 
 form.addEventListener('submit', onSubmit);
 
-// 1. получить данные от функции earchMovies из './trending-movies'
-// 2. сделать проверку полученых результатов
-// 3. отрисовать после проверки два варианта
-// 3.1.успех - новую галерею
-// 3.2. провал - выдать сообщение об ошибке
+async function onSubmit(event) {
+  event.preventDefault();
+  let searchQuery = input.value.trim();
+  if (searchQuery === '') {
+    return Notify.failure('Please enter name of the film.', {
+      timeout: 5000,
+      position: 'center-top',
+      width: 200,
+      showOnlyTheLastOne: true,
+    });
+  }
+  try {
+    preloaderShow();
+    // тут якась фигня
+    const data = await API_KEY.searchMovies(searchedText);
 
-// черновик
+    if (data.total_results === 0) {
+      input.value = '';
+      hidePreloader();
+      return Notify.failure(
+        'There is no films with this name. Please try again.',
+        {
+          timeout: 5000,
+          position: 'center-top',
+          width: 200,
+          showOnlyTheLastOne: true,
+        }
+      );
+    }
+    Notify.info('Your request is successfull.', {
+      timeout: 5000,
+      position: 'center-top',
+      width: 200,
+      showOnlyTheLastOne: true,
+    });
+    input.value = '';
+    hidePreloader();
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // preloaderShow();
-// const onSearch = new onSearch();
+// const onSubmit = new onSubmit();
 // const searchedText = '';
-// onSearch.searchMovies();
-// onSearch
+// onSubmit.searchMovies(значення з формы);
+// onSubmit
 //   .searchMovies(searchedText)
 //   .then(({ results, total_results, total_pages, page }) => {
 //     const markup = createMarkup(results);
 //     pagination(total_pages, page);
+//     // очистити вміст галереї
+
 //     refs.listFilm.insertAdjacentHTML('beforeend', markup);
 //     hidePreloader();
 //   });
 
-// function headMarkup(cards) {
-//   return cards.reduce((acc, card) => acc + headMarkup(card), '');
-// }
+// // function headMarkup(cards) {
+// //   return cards.reduce((acc, card) => acc + headMarkup(card), '');
+// // }
