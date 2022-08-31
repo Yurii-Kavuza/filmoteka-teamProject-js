9d1e81f03ab091e2c4813214f4b67b3620f6e782
Filatov44 <filatov300@gmail.com>
2022-08-31 22:53:46 +0300

    add after pagination go top

--- src/index.js
+++ src/index.js
@@ -48,6 +48,10 @@ const trendMovi = event => {
       refs.listFilm.innerHTML = '';
       refs.listFilm.insertAdjacentHTML('beforeend', markup);
       hidePreloader();
+     window.scrollTo(0, 200);
     });
 };
 paganation.on('afterMove', trendMovi);
+
+
+
