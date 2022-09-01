2b83c6fbc60972bdf862b2116316339c810885a4
Yurii-Kavuza <kavuzaiurii@gmail.com>
2022-09-01 07:24:44 +0300

    need to check

--- src/js/gallery-item.js
+++ src/js/gallery-item.js
@@ -1,5 +1,7 @@
 import { IMG_URL } from './trending-movies';
 
+const DEFAULT_IMG_PATH = 'https://poster.keepcalmandposters.com/4680205.jpg';
+
 const getGenreFullList = genres => genres.map(genre => genre.name);
 
 const getGenresShortList = genres => {
@@ -21,6 +23,7 @@ const headMarkup = item => {
   const imageOriginal = `${IMG_URL}/original${item.poster_path}`;
   const markup = `<li class="gallery__item" id="${item.id}">
   <a class="slot__thumb">
+  ${returnImageMarkup(item, 'img__gallery')}
             <img class="img__gallery" src="${imageOriginal}"
              alt="${item.title}"
              data-id="${item.id}"
@@ -43,7 +46,8 @@ const watchedMarkup = item => {
   const imageOriginal = `${IMG_URL}/original/${item.poster_path}`;
   const markup = `<li class="gallery__item" id="${item.id}">
   <a class="slot__thumb">
-            <img class="img__gallery" src="${imageOriginal}"
+  ${returnImageMarkup(item, 'img__gallery')}          
+  <img class="img__gallery" src="${imageOriginal}"
              alt="${item.title}"
              data-id="${item.id}"
              data-allGenres="${genresFullList}"
@@ -61,4 +65,44 @@ const watchedMarkup = item => {
   return markup;
 };
 
-export { headMarkup, watchedMarkup, createIndexMarkup };
+function returnImageMarkup(item, classImg) {
+  let imageMarkup = '';
+
+  if (item.poster_path) {
+    imageMarkup = `<picture>
+                <source
+                    srcset="
+                    ${IMG_URL}/w780${item.poster_path}     1x,
+                    ${IMG_URL}/original${item.poster_path} 2x
+                    "
+                    media="(min-width: 1024px)"
+                    
+                />
+                <source
+                    srcset="
+                    ${IMG_URL}/w500${item.poster_path} 1x,
+                    ${IMG_URL}/w780${item.poster_path} 2x
+                    "
+                    media="(min-width: 768px)"
+                />
+                <source
+                    srcset="
+                    ${IMG_URL}/w342${item.poster_path} 1x,
+                    ${IMG_URL}/w500${item.poster_path} 2x
+                    "
+                    media="(min-width: 320px)"
+                />
+                <img
+                    src="${IMG_URL}/original${item.poster_path}" loading="lazy"
+                    alt="${item.original_title}"
+                    class=${classImg}
+                    
+                />
+                </picture>`;
+  } else {
+    imageMarkup = `<img class=${classImg} loading="lazy" alt="${item.original_title}" src="${DEFAULT_IMG_PATH}"/>`;
+  }
+  return imageMarkup;
+}
+
+export { headMarkup, watchedMarkup, createIndexMarkup, returnImageMarkup };
