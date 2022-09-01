664e3dfde22f1cc65e15f3479f1dca05677f3ece
Filatov44 <filatov300@gmail.com>
2022-09-01 14:18:36 +0300

    fix my lib

--- src/my-library.js
+++ src/my-library.js
@@ -2,4 +2,4 @@ import { currentWatchedPage } from './js/header-library';
 import { currentQueuePage } from './js/header-library';
 import './js/library-watched-btn';
 import './js/modal-footer';
-import './js/modal-btns';
+// import './js/modal-btns';
